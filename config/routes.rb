9ad8329bcaa2423756317ba9034689918f5b3c17
Chandra Lama <iamme24cl@gmail.com>
2021-05-12 14:39:41 -0700

    implemented nested routed for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :accounts, only: [:index, :show, :create, :update] do
+        resources :invoices, only: [:index, :show, :create, :update, :destroy] 
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
